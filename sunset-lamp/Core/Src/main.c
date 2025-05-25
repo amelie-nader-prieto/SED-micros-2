010821586997fd97f09a7086cb4beb342fc638dc
Amélie <amelie.nader.prieto@alumnos.upm.es>
2025-05-25 16:53:32 +0200

    Programar potenciómetro
    Se ha programado el potenciómetro que modifica el brillo

--- sunset-lamp/Core/Src/main.c
+++ sunset-lamp/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stm32f4xx_hal_tim.h"
+#include "stm32f4xx_hal_adc.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -34,6 +35,8 @@
 #define CANAL_R TIM_CHANNEL_1 //rojo
 #define CANAL_G TIM_CHANNEL_2 //verde
 #define CANAL_B TIM_CHANNEL_3 // azul
+#define MAX_ADC 4095
+#define MAX_PWM 999
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -42,18 +45,37 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN PV */
-
+volatile uint32_t brillo = 0;
+volatile uint32_t brillo_raw = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
+uint32_t scale(uint32_t raw){
+	return (uint32_t)((raw*MAX_PWM)/MAX_ADC);
+}
+uint32_t leer_adc1(){
 
+	HAL_ADC_Start(&hadc1);
+	if(HAL_ADC_PollForConversion(&hadc1, 100) == HAL_OK){
+		//return HAL_ADC_GetValue(&hadc1);
+		uint32_t raw = HAL_ADC_GetValue(&hadc1);
+		HAL_ADC_Stop(&hadc1);
+		return raw;
+	}
+	HAL_ADC_Stop(&hadc1);
+	return -1;
+
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -91,6 +113,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   //HAL_TIM_PWM_Init(&htim1);
@@ -99,23 +122,23 @@ int main(void)
   HAL_TIM_PWM_Start(&htim1, CANAL_B);
   __HAL_TIM_MOE_ENABLE(&htim1);
 
+  HAL_ADC_Start(&hadc1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  //__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,0);
-	  //HAL_Delay(1000);
-	  __HAL_TIM_SET_COMPARE(&htim1,CANAL_R,600);
-	  __HAL_TIM_SET_COMPARE(&htim1,CANAL_G,600);
-	  __HAL_TIM_SET_COMPARE(&htim1,CANAL_B,600);
-	  HAL_Delay(5000);
-	  //__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,990);
-	  //HAL_Delay(5000);
+	  __HAL_TIM_SET_COMPARE(&htim1,CANAL_R,brillo);
+	  __HAL_TIM_SET_COMPARE(&htim1,CANAL_G,brillo);
+	  __HAL_TIM_SET_COMPARE(&htim1,CANAL_B,brillo);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  // Leer el ADC por polling
+	  brillo_raw = leer_adc1();
+	  brillo = scale(brillo_raw);// brillo_raw * (MAX_PWM/MAX_ADC);
   }
   /* USER CODE END 3 */
 }
@@ -164,6 +187,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
